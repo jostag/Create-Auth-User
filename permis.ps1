40b52355c7f209fa13a531d9ce1760e88f45cf1f
jostag <35642154+jostag@users.noreply.github.com>
2021-07-21 10:23:31 -0400

    Add files via upload

---
+++ permis.ps1
@@ -0,0 +1,37 @@
+﻿function filePerm{
+
+$acl = Get-Acl C:\
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Users","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.AddAccessRule($rule)
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Authenticated Users","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.AddAccessRule($rule)
+(Get-Item C:\).SetAccessControl($acl)
+
+$acl = Get-Acl 'C:\Program Files\'
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Users","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.AddAccessRule($rule)
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Authenticated Users","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.AddAccessRule($rule)
+(Get-Item 'C:\Program Files\').SetAccessControl($acl)
+
+$acl = Get-Acl 'C:\ProgramData\'
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Users","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.AddAccessRule($rule)
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Authenticated Users","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.AddAccessRule($rule)
+(Get-Item 'C:\ProgramData\').SetAccessControl($acl)
+
+$acl = Get-Acl 'C:\Program Files (x86)\'
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Users","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.AddAccessRule($rule)
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Authenticated Users","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.AddAccessRule($rule)
+(Get-Item 'C:\Program Files (x86)\').SetAccessControl($acl)
+
+$acl = Get-Acl 'C:\Windows\Temp'
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Users","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.AddAccessRule($rule)
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Authenticated Users","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$acl.AddAccessRule($rule)
+(Get-Item 'C:\Windows\Temp').SetAccessControl($acl)
+}
