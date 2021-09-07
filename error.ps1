faaecf5bdc4d4f88862dce3049b3e9d6a818fe8c
jostag <35642154+jostag@users.noreply.github.com>
2021-09-06 21:06:45 -0400

    Add files via upload

---
+++ error.ps1
@@ -0,0 +1,11 @@
+﻿ start-process -WindowStyle Hidden powershell.exe C:\error.ps1
+    
+ function error{   
+    Add-Type -AssemblyName PresentationCore,PresentationFramework
+    sleep 30
+    $button = [System.Windows.MessageBoxButton]::OK
+    $icon = [System.Windows.MessageBoxImage]::Error
+    $message = "PC Load Letter"
+    $title = "Error"
+    [System.Windows.MessageBox]::Show($message,$title,$button,$icon)
+    }
